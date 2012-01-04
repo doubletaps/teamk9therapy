5655e334c1fe2f232b02e1ef7ba95fe46dde4cd9
Tom Shires <tom@doubletaps.net>
2012-01-03 20:13:24 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pettherapy::Application.config.secret_token = '61d7c3b084f1f3a13bd08c9ef176aa47259360b34e873692ab6c5d66c8c21ef418b21b6b19a7727672ac26055ae5e68898e2eb674694866919bba9c4c5464b03'
